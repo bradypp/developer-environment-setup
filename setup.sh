8527c1c67e137888e8abfabc583336e59555c6a9
bradypp <bradypp44@gmail.com>
2020-06-12 13:50:03 +0100

    Script updates

--- setup.sh
+++ setup.sh
@@ -1,28 +1,25 @@
+# Installing essentials
 sudo apt-get update
 sudo apt -y upgrade
+sudo apt-get install -y build-essential libssl-dev
 sudo apt-get install curl
 
-# Installing build essentials
-sudo apt-get install -y build-essential libssl-dev
-
-# Git - a version control system
-sudo apt-get update
+# Git
 sudo apt install git-all
 
 # Zsh
 sudo apt-get -y install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo npm install -g spaceship-prompt
+
+# Nodejs and NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+nvm install --lts
 
 # Update config files
 cp ./root/.gitconfig ~/.gitconfig
 cp ./root/.zshrc ~/.zshrc
 source ~/.zshrc
-
-# Nodejs and NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
-nvm install --lts
 
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -33,9 +30,12 @@ sudo apt install --no-install-recommends yarn
 sudo apt install snapd
 
 # CLIs & Packages
-sudo npm install -g nodemon
-sudo npm install -g grunt-cli
-sudo npm install -g gulp-cli
-sudo npm install -g gatsby-cli
-sudo npm install -g netlify-cli
-sudo snap install --classic heroku
+npm i -g nodemon
+npm i -g grunt-cli
+npm i -g gulp-cli
+npm i -g gatsby-cli
+npm i -g netlify-cli
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+# Cleanup
+sudo apt autoremove
